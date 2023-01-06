2ac78c8a641e844fa3f85cbc22aba9ccc160d684
Jihanetbh <jihaneammaoui659@gmail.com>
2023-01-06 14:23:05 +0100

    2

--- app/src/main/java/com/example/blithe/MainActivity.kt
+++ app/src/main/java/com/example/blithe/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.welcome_to_blithe)
+        setContentView(R.layout.sidebar)
     }
 }
