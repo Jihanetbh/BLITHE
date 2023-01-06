88aedd7d82cb5b9241de406ac6a733fd5506c61c
Jihanetbh <jihaneammaoui659@gmail.com>
2023-01-06 18:18:15 +0100

    3

--- app/src/main/java/com/example/blithe/SignInAct.java
+++ app/src/main/java/com/example/blithe/SignInAct.java
@@ -1,12 +1,31 @@
 package com.example.blithe;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignInAct extends AppCompatActivity {
+
+    private ImageButton back2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
+        back2 = (ImageButton) findViewById(R.id.flecheBack2);
+
+        back2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goback2();
+            }
+        });
+    }
+
+    public void goback2() {
+        Intent intent = new Intent(this, signupsigninact.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/blithe/SignUpAct.java
+++ app/src/main/java/com/example/blithe/SignUpAct.java
@@ -1,12 +1,30 @@
 package com.example.blithe;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpAct extends AppCompatActivity {
+    private ImageButton back1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
-        }
+        back1 = (ImageButton) findViewById(R.id.flecheBack);
+
+        back1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goback();
+            }
+        });
+    }
+
+    public void goback() {
+        Intent intent = new Intent(this, signupsigninact.class);
+        startActivity(intent);
+    }
 }
